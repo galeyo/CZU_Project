c30c0c023cee8adc3529e2991b4f05884bff736e
Galey <nickokhapkin@gmail.com>
2018-12-01 13:21:22 +0100

    add adaptive to navigation menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-var mediaCond = window.matchMedia("(min-width: 992px)")
+function changeNavClassName() {
+	var nav = document.querySelector(".navbar-list-link");
+	if(nav.className === "navbar-list-link") {
+		nav.className += " responsive";
+	} else {
+		nav.className = "navbar-list-link";
+	} 
+}
+
+window.onscroll = () => scrollFunction();
+
+function scrollFunction() {
+	if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.querySelector(".navbar").style.backgroundColor = "#ffffff";
+	} else {
+		document.querySelector(".navbar").style.backgroundColor = "";
+	}
+}
